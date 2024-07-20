9bc33961a3720040700f4001523f5a10744a428f
ravindra <react2@flyweis.technology>
2024-07-20 17:28:52 +0530

    updated

--- src/Pages/PaymentSuccess.js
+++ src/Pages/PaymentSuccess.js
@@ -3,7 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 const PaymentSuccess = () => {
   const navigate = useNavigate();
-
+  const redirectToHome = () => {
+    if (window.top !== window.self) {
+      window.top.location.replace("/home");
+    } else {
+      window.location.href = "/home";
+    }
+  };
   return (
     <div className="bg-slate-100 h-screen flex justify-center">
       <div className="grid place-items-center">
@@ -11,7 +17,7 @@ const PaymentSuccess = () => {
           <div className="bg-[#FFDC82] w-[500px] p-5 flex justify-center items-center text-xl font-semibold top-0 fixed z-50"></div>
           <div className="withdraw-complete-div">
             <p>Your transaction has been successfully completed.</p>
-            <button onClick={() => navigate("/home")}>Home</button>
+            <button onClick={redirectToHome}>Home</button>
           </div>
         </div>
       </div>
